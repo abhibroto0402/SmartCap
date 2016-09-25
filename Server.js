1439e6b8af50142a2e9eb6e188dbf74beda598f3
Abhibroto Mukherjee <abhibroto04@gmail.com>
2016-09-25 06:50:39 +0000

    Server JS updated as 2 listner event existed in code. Resulting in server crashes

--- Server.js
+++ Server.js
@@ -3,13 +3,14 @@ var app = express();
 var port = process.env.PORT;
 var path = require('path');
 
+console.log(port);
+
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname,'client', 'views'));
-app.listen(port);
 
 app.get('/', function(req, res){
 	res.render('index.ejs');
 });
-app.listen(port, function(){
+app.listen(port, process.env.IP, function(){
 console.log("Server UP: Port :" + port);
 });
